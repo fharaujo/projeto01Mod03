93b23a2eeb3b6b155e1f72efc3fe5a058616e92c
fharaujo <faraujodeveloper@hotmail.com>
2021-08-27 17:29:10 -0300

    update game and index database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/db_filmes", {
+mongoose.connect("mongodb://localhost:27017/db_project", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 },  (error) =>{
